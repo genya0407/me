e805ab1930cf89c87ed4c75f0c603aea92282b58
Yusuke Sangenya <longinus.eva@gmail.com>
2017-06-03 16:56:19 +0900

    renewal

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem 'rakyll'
 gem 'qiita'
+gem 'filewatcher'
---
+++ watch.sh
@@ -0,0 +1,1 @@
+bundle exec filewatcher index.html products/* assets/*/* templates/* site.rb "bundle exec ruby site.rb"

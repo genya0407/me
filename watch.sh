f4b6eac3cdc07ff9e66b5a9ddb749465ae1a8d00
Yusuke Sangenya <longinus.eva@gmail.com>
2017-09-11 23:58:17 +0900

    update

--- watch.sh
+++ watch.sh
@@ -1,2 +1,3 @@
 bundle exec ruby site.rb
-bundle exec filewatcher index.html products/* assets/*/* templates/* site.rb "bundle exec ruby site.rb"
+bundle exec filewatcher index.html products/* assets/*/* templates/* site.rb "bundle exec ruby site.rb" &
+ruby -rwebrick -e 'WEBrick::HTTPServer.new(:DocumentRoot => "./_site", :Port => 8000).start'
